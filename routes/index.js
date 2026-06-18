5da86ed6ca9016f6e8e16b779a4c99c957cdde0a
Yoshiki hashiba <j2330@matsue-ct.ac.jp>
2026-06-18 06:59:14 +0000

    ToDoタスクをデータベースから取得して表示させる処理を実装しよう。

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,24 @@ let todos = [];
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '[事前準備で設定したrootユーザのパスワード]',
+  password: 'password',
   database: 'todo_app'
 });
 
 todos = [];
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  connection.query(
+    `select * from tasks;`,
+    (error, results) => {
+      console.log(error);
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    }
+  );
 });
 
 router.post('/', function (req, res, next) {

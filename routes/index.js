a2c755970e70ca9dca7fbe6b53f1932b0a9ea4ed
Yoshiki hashiba <j2330@matsue-ct.ac.jp>
2026-07-02 04:59:40 +0000

    mysql接続設定

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '[事前準備で設定したrootユーザのパスワード]',
+  database: 'todo_app'
+});
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
@@ -26,6 +34,13 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
   knex("tasks")
     .insert({user_id: 1, content: todo})
